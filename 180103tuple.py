681c8012821e42c4996bf4c4e38395fbb2d568f5
dane <dane.liu@huolala.cn>
2018-01-03 20:28:49 +0800

    Python
    Hello

---
+++ 180103tuple.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+#print apple
+print(L[0][0])
+
+#print python
+print(L[1][1])
+
+#print lisa
+
+print(L[2][2])
+
+
+
+height = 1.75
+weight = 80.5
+BMI= weight/(height*height)
+if BMI < 18.5:
+    print 'light'
+elif BMI >=18.5 and <25:
+    print 'normal‘
+e l
