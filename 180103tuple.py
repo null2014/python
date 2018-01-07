142de0581294765858d0ba43a41df2eb816997f4
dane <2289975022@qq.com>
2018-01-08 07:08:09 +0800

    test

--- 180103tuple.py
+++ 180103tuple.py
@@ -43,3 +43,24 @@ n2 = 1000
 s = hex(n2)
 print(s)
 
+
+
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+
+#異動一個點倒另一個點，得出新點的座標
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny    
+
+#请定义一个函数quadratic(a, b, c)，接收3个参数，返回一元二次方程：ax2 + bx + c = 0的两个解
+
+import math
+
+def quadratic(a, b, c):
+    pass
